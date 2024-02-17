49a505f8c5312e3d3f1173951ee183c2f45f258e
Amstrong1 <lewisamstrongh@gmail.com>
2024-02-17 13:25:23 +0100

    email upd

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Http\Requests\ContactStoreRequest;
 
@@ -16,6 +18,20 @@ class ContactController extends Controller
             'object' => $request->subject,
             'message' => $request->message,
         ]);
+
+        $message = new Contact();
+        $message->name = $request->name;
+        $message->email = $request->email;
+        $message->object = $request->subject;
+        $message->message = $request->message;
+        $message->save();
+        
+        $voltaire = 'voltairefadonougbo@gmail.com';
+        $romuald = 'romuald91303142@gmail.com';
+        $lewis = 'lewishoundje@gmail.com';
+        Mail::to($voltaire)->send(new ContactMail($message));
+        Mail::to($romuald)->send(new ContactMail($message));
+        Mail::to($lewis)->send(new ContactMail($message));
         
         Alert::toast('Merci de nous avoir contacter. Vous recevrez une réponse sous peu', 'success');
         return redirect()->back();
